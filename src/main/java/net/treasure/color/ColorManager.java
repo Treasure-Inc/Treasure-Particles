5f068c92601d206ad9ffc602132200e390d44146
ItsZypec <itszypec@gmail.com>
2022-08-10 13:39:56 +0300

    new colors

--- src/main/java/net/treasure/color/ColorManager.java
+++ src/main/java/net/treasure/color/ColorManager.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class ColorManager implements DataHolder {
 
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
     final ConfigurationGenerator generator;
 
     @Getter
