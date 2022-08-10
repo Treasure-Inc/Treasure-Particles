a8ca0ef2061d2acbb05f0fa532fe9ba7da5fd20a
ItsZypec <itszypec@gmail.com>
2022-08-10 13:46:36 +0300

    bump version

--- src/main/java/net/treasure/core/notification/NotificationManager.java
+++ src/main/java/net/treasure/core/notification/NotificationManager.java
@@ -22,7 +22,8 @@ public class NotificationManager {
             "1.2.0",
             "1.2.1",
             "1.2.2",
-            "1.2.3"
+            "1.2.3",
+            "1.3.0"
     );
 
     // Needs rework
--- src/main/java/net/treasure/effect/EffectManager.java
+++ src/main/java/net/treasure/effect/EffectManager.java
@@ -36,7 +36,7 @@ import java.util.logging.Level;
 @Getter
 public class EffectManager implements DataHolder {
 
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.3.0";
 
     ConfigurationGenerator generator;
 
