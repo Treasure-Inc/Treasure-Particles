247f738d7c446289371ce4dfc8bb92140c6013c5
ItsZypec <itszypec@gmail.com>
2023-02-03 03:00:57 +0300

    rainbow colors size option

--- src/main/java/net/treasure/color/ColorManager.java
+++ src/main/java/net/treasure/color/ColorManager.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class ColorManager implements DataHolder {
 
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.4.0";
     final ConfigurationGenerator generator;
 
     @Getter
@@ -81,7 +81,7 @@ public class ColorManager implements DataHolder {
         }
 
         ColorScheme rainbow = new ColorScheme("rainbow");
-        rainbow.getColors().addAll(Arrays.asList(new Rainbow().colors(25)));
+        rainbow.getColors().addAll(Arrays.asList(new Rainbow().colors(config.getInt("rainbow-colors-size", 25))));
         colors.add(rainbow);
     }
 
