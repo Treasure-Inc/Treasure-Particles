e29ec25114a5849e6ec1d2a63b830dc9b17f1aec
ItsZypec <itszypec@gmail.com>
2023-04-12 02:19:03 +0300

    updated dot particle spawner

--- plugin/src/main/java/net/treasure/effect/script/particle/style/DotParticle.java
+++ plugin/src/main/java/net/treasure/effect/script/particle/style/DotParticle.java
@@ -5,9 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import net.treasure.color.data.ColorData;
+import net.treasure.common.particles.ParticleEffect;
 import net.treasure.effect.TickHandler;
 import net.treasure.effect.data.EffectData;
-import net.treasure.effect.script.argument.type.BooleanArgument;
 import net.treasure.effect.script.argument.type.FloatArgument;
 import net.treasure.effect.script.argument.type.IntArgument;
 import net.treasure.effect.script.argument.type.RangeArgument;
@@ -15,9 +15,9 @@ import net.treasure.effect.script.argument.type.VectorArgument;
 import net.treasure.effect.script.particle.ParticleOrigin;
 import net.treasure.effect.script.particle.ParticleSpawner;
 import net.treasure.util.Vectors;
+import net.treasure.util.particles.Particles;
 import org.bukkit.entity.Player;
-import xyz.xenondevs.particle.ParticleEffect;
-import xyz.xenondevs.particle.data.ParticleData;
+import org.bukkit.util.Vector;
 
 @Getter
 @Setter
@@ -29,34 +29,41 @@ public class DotParticle extends ParticleSpawner {
 
     public DotParticle(ParticleEffect effect, ParticleOrigin origin,
                        VectorArgument position, VectorArgument offset,
-                       ColorData colorData, ParticleData particleData,
-                       IntArgument amount, FloatArgument multiplier, RangeArgument speed, RangeArgument size, BooleanArgument directional) {
+                       ColorData colorData, Object particleData,
+                       IntArgument amount, FloatArgument multiplier, RangeArgument speed, RangeArgument size, boolean directional) {
         super(effect, origin, position, colorData, particleData, amount, multiplier, speed, size, directional);
         this.offset = offset;
     }
 
     @Override
     public TickResult tick(Player player, EffectData data, TickHandler handler, int times) {
-        var builder = tick(player, data);
-        if (builder == null) return TickResult.NORMAL;
+        var context = tick(player, data);
+        if (context == null) return TickResult.NORMAL;
+
+        var builder = context.builder();
+        var origin = context.origin();
+
+        var vector = position == null ? new Vector(0, 0, 0) : position.get(player, data);
+        builder.location(rotate(player, origin, vector));
 
         var offset = this.offset != null ? this.offset.get(player, data) : null;
-
-        if (directional.get(player, data) && offset != null) {
+        if (directional && offset != null) {
             offset = Vectors.rotateAroundAxisX(offset, player.getEyeLocation().getPitch());
             offset = Vectors.rotateAroundAxisY(offset, player.getEyeLocation().getYaw());
             offset = offset.add(player.getLocation().getDirection().add(offset));
         }
 
         if (offset != null)
-            builder.setOffset(offset);
+            builder.offset(offset);
 
-        display(builder);
+        updateParticleData(player, data, builder);
+
+        Particles.send(builder);
         return TickResult.NORMAL;
     }
 
     @Override
     public DotParticle clone() {
-        return new DotParticle(effect, origin, position, offset, colorData, particleData, amount, multiplier, speed, size, directional);
+        return new DotParticle(particle, origin, position, offset, colorData, particleData, amount, multiplier, speed, size, directional);
     }
 }
