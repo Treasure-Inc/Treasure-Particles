8c6e5621b827808d870657fc320d3922257d6676
ItsZypec <itszypec@gmail.com>
2023-04-12 01:59:06 +0300

    color selection GUI

---
+++ plugin/src/main/java/net/treasure/core/gui/type/color/ColorsGUIHolder.java
@@ -0,0 +1,12 @@
+package net.treasure.core.gui.type.color;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.treasure.core.gui.GUIHolder;
+import net.treasure.effect.Effect;
+
+@Getter
+@AllArgsConstructor
+public class ColorsGUIHolder extends GUIHolder {
+    Effect effect;
+}
