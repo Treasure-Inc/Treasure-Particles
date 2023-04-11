08e3e910f07c2f8c92d22a2e2e89df0225b89e2f
ItsZypec <itszypec@gmail.com>
2023-04-12 01:13:04 +0300

    1_16_R2

---
+++ v1_16_R2/src/main/java/net/treasure/version/data/color/NMSDustTransitionData.java
@@ -0,0 +1,16 @@
+package net.treasure.version.data.color;
+
+import net.treasure.common.particles.data.color.ParticleDustTransitionData;
+import org.bukkit.Color;
+
+public class NMSDustTransitionData extends ParticleDustTransitionData {
+
+    public NMSDustTransitionData(Color color, Color transition, float size) {
+        super(color, transition, size);
+    }
+
+    @Override
+    public Object toNMS() {
+        return null;
+    }
+}
