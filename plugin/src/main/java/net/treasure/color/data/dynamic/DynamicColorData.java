7e5b97a8f2efba3bef7c6027cf489c944f7cf591
ItsZypec <itszypec@gmail.com>
2023-04-12 01:57:07 +0300

    dynamic color data

---
+++ plugin/src/main/java/net/treasure/color/data/dynamic/DynamicColorData.java
@@ -0,0 +1,19 @@
+package net.treasure.color.data.dynamic;
+
+import net.treasure.color.data.ColorData;
+import net.treasure.effect.data.EffectData;
+import org.bukkit.Color;
+
+public class DynamicColorData extends ColorData {
+
+    public DynamicColorData(float speed, boolean revertWhenDone, boolean stopCycle) {
+        super(speed, revertWhenDone, stopCycle);
+    }
+
+    @Override
+    public Color next(EffectData data) {
+        var colors = data.getColorPreferences().get(data.getCurrentEffect().getKey()).getColors();
+        max = colors.size();
+        return colors.get(index());
+    }
+}
