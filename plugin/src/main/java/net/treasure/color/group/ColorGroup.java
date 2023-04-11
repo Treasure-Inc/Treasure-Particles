0e69e195cb1faea4dec490c0f44a354fb74bace0
ItsZypec <itszypec@gmail.com>
2023-04-12 01:48:31 +0300

    added color groups

---
+++ plugin/src/main/java/net/treasure/color/group/ColorGroup.java
@@ -0,0 +1,36 @@
+package net.treasure.color.group;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import net.treasure.color.ColorScheme;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ColorGroup {
+    String key;
+    List<Option> availableOptions;
+
+    public Option getOption(ColorScheme colorScheme) {
+        return availableOptions.stream().filter(option -> option.colorScheme.equals(colorScheme)).findFirst().orElse(null);
+    }
+
+    public boolean hasOption(ColorScheme colorScheme) {
+        return availableOptions.stream().anyMatch(option -> option.colorScheme.equals(colorScheme));
+    }
+
+    @Getter
+    @Accessors(fluent = true)
+    @AllArgsConstructor
+    public static class Option {
+        ColorScheme colorScheme;
+        String permission;
+
+        public boolean canUse(Player player) {
+            return permission == null || player.hasPermission(permission);
+        }
+    }
+}
