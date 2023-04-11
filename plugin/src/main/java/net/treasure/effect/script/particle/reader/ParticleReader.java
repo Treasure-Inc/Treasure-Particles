7baba6ba023b3417ecf1f3ea0a073d9065f02579
ItsZypec <itszypec@gmail.com>
2023-04-12 02:21:05 +0300

    updated particle reader

--- plugin/src/main/java/net/treasure/effect/script/particle/reader/ParticleReader.java
+++ plugin/src/main/java/net/treasure/effect/script/particle/reader/ParticleReader.java
@@ -3,8 +3,10 @@ package net.treasure.effect.script.particle.reader;
 import net.treasure.color.data.ColorData;
 import net.treasure.color.data.SingleColorData;
 import net.treasure.common.Patterns;
+import net.treasure.common.particles.ParticleEffect;
 import net.treasure.effect.Effect;
 import net.treasure.effect.exception.ReaderException;
+import net.treasure.effect.script.ReaderContext;
 import net.treasure.effect.script.ScriptReader;
 import net.treasure.effect.script.argument.type.BooleanArgument;
 import net.treasure.effect.script.argument.type.FloatArgument;
@@ -15,22 +17,28 @@ import net.treasure.effect.script.argument.type.StaticArgument;
 import net.treasure.effect.script.argument.type.VectorArgument;
 import net.treasure.effect.script.particle.ParticleOrigin;
 import net.treasure.effect.script.particle.ParticleSpawner;
-import xyz.xenondevs.particle.ParticleEffect;
-import xyz.xenondevs.particle.PropertyType;
-import xyz.xenondevs.particle.data.SculkChargeData;
-import xyz.xenondevs.particle.data.ShriekData;
-import xyz.xenondevs.particle.data.texture.BlockTexture;
-import xyz.xenondevs.particle.data.texture.ItemTexture;
+import net.treasure.util.particles.Particles;
+import org.bukkit.Bukkit;
 
-import java.awt.*;
 import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ParticleReader<T extends ParticleSpawner> extends ScriptReader<DotParticleReaderContext, T> {
+public abstract class ParticleReader<T extends ParticleSpawner> extends ScriptReader<ParticleReader.Context<T>, T> {
 
     public ParticleReader() {
-        addValidArgument(c -> c.script().effect(StaticArgument.asEnum(c, ParticleEffect.class)), "effect");
+        addValidArgument(c -> {
+            var particle = StaticArgument.asEnum(c, ParticleEffect.class);
+            if (particle == null || particle.bukkit() == null) {
+                if (c.value().equalsIgnoreCase("vibration")) {
+                    error(c, "Vibration effect is not supported");
+                    return;
+                }
+                error(c, "Unknown particle effect: " + c.value());
+                return;
+            }
+            c.script().particle(particle);
+        }, "effect");
 
         addValidArgument(c -> {
             var args = Patterns.ASTERISK.split(c.value());
@@ -48,27 +56,27 @@ public class ParticleReader<T extends ParticleSpawner> extends ScriptReader<DotP
             }
         }, "origin");
 
-        addValidArgument(c -> c.script().position(VectorArgument.read(c)), "pos");
+        addValidArgument(c -> c.script().position(VectorArgument.read(c)), "pos", "position");
 
         addValidArgument(c -> {
-            var particle = c.script().effect();
-            if (particle != null && !particle.hasProperty(PropertyType.COLORABLE)) {
-                error(c, "You cannot use '" + c.type() + "' with this particle effect: " + particle.name());
+            var particle = c.script().particle();
+            if (particle != null && !particle.hasProperty(ParticleEffect.Property.CAN_BE_COLORED)) {
+                error(c, "You cannot use '" + c.key() + "' with this particle effect: " + particle.name());
                 return;
             }
             try {
                 c.script().colorData(ColorData.fromString(c.value()));
             } catch (ReaderException e) {
                 try {
-                    c.script().colorData(new SingleColorData(Color.decode("#" + c.value())));
+                    c.script().colorData(new SingleColorData("#" + c.value()));
                     return;
                 } catch (Exception ignored) {
                 }
                 error(c, e.getMessage());
             }
-        }, "color", "colorscheme");
+        }, "color", "color-scheme");
 
-        addValidArgument(c -> c.script().directional(BooleanArgument.read(c)), "direction", "directional");
+        addValidArgument(c -> c.script().directional(BooleanArgument.read(c.value())), "direction", "directional");
 
         addValidArgument(c -> c.script().amount(IntArgument.read(c)), "amount");
 
@@ -77,66 +85,68 @@ public class ParticleReader<T extends ParticleSpawner> extends ScriptReader<DotP
         addValidArgument(c -> c.script().size(RangeArgument.read(c)), "size");
 
         addValidArgument(c -> {
-            var particle = c.script().effect();
-            if (particle != null && !particle.hasProperty(PropertyType.REQUIRES_ITEM)) {
-                error(c, "You cannot use 'item' with this particle effect: " + particle.name());
+            var particle = c.script().particle();
+            if (particle != null && !particle.hasProperty(ParticleEffect.Property.REQUIRES_ITEM)) {
+                error(c, "You cannot use '" + c.key() + "' with this particle effect: " + particle.name());
                 return;
             }
             var item = ItemStackArgument.read(c);
             if (item == null) return;
-            c.script().particleData(new ItemTexture(item));
+            c.script().particleData(Particles.NMS.getGenericData(particle, item));
         }, "item");
 
         addValidArgument(c -> {
-            var particle = c.script().effect();
-            if (particle != null && !particle.hasProperty(PropertyType.REQUIRES_BLOCK)) {
-                error(c, "You cannot use 'block' with this particle effect: " + particle.name());
+            var particle = c.script().particle();
+            if (particle != null && !particle.hasProperty(ParticleEffect.Property.REQUIRES_BLOCK)) {
+                error(c, "You cannot use '" + c.key() + "' with this particle effect: " + particle.name());
                 return;
             }
             var item = ItemStackArgument.read(c);
             if (item == null) return;
-            c.script().particleData(new BlockTexture(item.getType()));
+            c.script().particleData(Particles.NMS.getGenericData(particle, Bukkit.createBlockData(item.getType())));
         }, "block");
 
         addValidArgument(c -> {
-            var particle = c.script().effect();
+            var particle = c.script().particle();
             if (particle != null && !particle.equals(ParticleEffect.SHRIEK)) {
-                error(c, "You can only use 'delay' with SHRIEK particle effect.");
+                error(c, "You can only use '" + c.key() + "' with SHRIEK particle effect.");
                 return;
             }
             try {
-                c.script().particleData(new ShriekData(StaticArgument.asInt(c)));
+                c.script().particleData(Particles.NMS.getGenericData(particle, StaticArgument.asInt(c)));
             } catch (ReaderException e) {
-                error(c, "Unexpected 'delay' value: " + c.value(), e.getMessage());
+                error(c, "Unexpected '" + c.key() + "' value: " + c.value(), e.getMessage());
             } catch (Exception ignored) {
-                error(c, "Unexpected 'delay' value: " + c.value());
+                error(c, "Unexpected '" + c.key() + "' value: " + c.value());
             }
         }, "delay");
 
         addValidArgument(c -> {
-            var particle = c.script().effect();
+            var particle = c.script().particle();
             if (particle != null && !particle.equals(ParticleEffect.SCULK_CHARGE)) {
-                error(c, "You can only use 'roll' with SCULK_CHARGE particle effect.");
+                error(c, "You can only use '" + c.key() + "' with SCULK_CHARGE particle effect.");
                 return;
             }
             try {
-                c.script().particleData(new SculkChargeData(StaticArgument.asFloat(c)));
+                c.script().particleData(Particles.NMS.getGenericData(particle, StaticArgument.asFloat(c)));
             } catch (ReaderException e) {
-                error(c, "Unexpected 'roll' value: " + c.value(), e.getMessage());
+                error(c, "Unexpected '" + c.key() + "' value: " + c.value(), e.getMessage());
             } catch (Exception ignored) {
-                error(c, "Unexpected 'roll' value: " + c.value());
+                error(c, "Unexpected '" + c.key() + "' value: " + c.value());
             }
         }, "roll");
     }
 
+    public abstract Context<T> createParticleReaderContext(Effect effect, String type, String line);
+
     @Override
-    public DotParticleReaderContext createContext(Effect effect, String type, String line) {
-        return new DotParticleReaderContext(effect, type, line);
+    public Context<T> createContext(Effect effect, String type, String line) {
+        return createParticleReaderContext(effect, type, line);
     }
 
     @Override
-    public boolean validate(DotParticleReaderContext context) throws ReaderException {
-        if (context.script().effect() == null) {
+    public boolean validate(Context<T> context) throws ReaderException {
+        if (context.script().particle() == null) {
             error(context.effect(), context.type(), context.line(), "You must define an 'effect' value");
             return false;
         }
@@ -146,5 +156,11 @@ public class ParticleReader<T extends ParticleSpawner> extends ScriptReader<DotP
             return false;
         }
         return true;
+    }
+
+    public static abstract class Context<S extends ParticleSpawner> extends ReaderContext<S> {
+        public Context(Effect effect, String type, String line, S particleSpawner) {
+            super(effect, type, line, particleSpawner);
+        }
     }
 }
