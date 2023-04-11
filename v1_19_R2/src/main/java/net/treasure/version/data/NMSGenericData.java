222a83a874dbc678157871c760d5aa2bb471f201
ItsZypec <itszypec@gmail.com>
2023-04-12 01:10:18 +0300

    1_19_R2

---
+++ v1_19_R2/src/main/java/net/treasure/version/data/NMSGenericData.java
@@ -0,0 +1,19 @@
+package net.treasure.version.data;
+
+import net.treasure.common.particles.ParticleEffect;
+import net.treasure.common.particles.data.ParticleGenericData;
+import org.bukkit.craftbukkit.v1_19_R2.CraftParticle;
+
+public class NMSGenericData extends ParticleGenericData {
+
+    public NMSGenericData(ParticleEffect effect, Object object) {
+        super(effect, object);
+    }
+
+    @Override
+    public Object toNMS() {
+        var particle = particleEffect.bukkit();
+        if (particle == null) return null;
+        return CraftParticle.toNMS(particle, object);
+    }
+}
