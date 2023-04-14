6fa33a8497f98c5ea4f4ff55147e73bf918d0b78
ItsZypec <itszypec@gmail.com>
2023-04-14 15:04:04 +0300

    improved vector rotating

--- plugin/src/main/java/net/treasure/util/Vectors.java
+++ plugin/src/main/java/net/treasure/util/Vectors.java
@@ -7,22 +7,20 @@ public class Vectors {
 
     public static Vector rotateAroundAxisX(Vector v, double angle) {
         angle = Math.toRadians(angle);
-        double y, z, cos, sin;
-        cos = MathUtils.cos(angle);
-        sin = MathUtils.sin(angle);
-        y = v.getY() * cos - v.getZ() * sin;
-        z = v.getY() * sin + v.getZ() * cos;
+        var cos = MathUtils.cos(angle);
+        var sin = MathUtils.sin(angle);
+        var y = v.getY() * cos - v.getZ() * sin;
+        var z = v.getY() * sin + v.getZ() * cos;
         return v.setY(y).setZ(z);
     }
 
     public static Vector rotateAroundAxisY(Vector v, double angle) {
         angle = -angle;
         angle = Math.toRadians(angle);
-        double x, z, cos, sin;
-        cos = MathUtils.cos(angle);
-        sin = MathUtils.sin(angle);
-        x = v.getX() * cos + v.getZ() * sin;
-        z = v.getX() * -sin + v.getZ() * cos;
+        var cos = MathUtils.cos(angle);
+        var sin = MathUtils.sin(angle);
+        var x = v.getX() * cos + v.getZ() * sin;
+        var z = v.getX() * -sin + v.getZ() * cos;
         return v.setX(x).setZ(z);
     }
 }
