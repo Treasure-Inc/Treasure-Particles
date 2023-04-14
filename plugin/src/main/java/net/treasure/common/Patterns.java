e6b779b4fca31215eca6d2a20b0b088a1bcab119
ItsZypec <itszypec@gmail.com>
2023-04-14 15:22:07 +0300

    updated patterns

--- plugin/src/main/java/net/treasure/common/Patterns.java
+++ plugin/src/main/java/net/treasure/common/Patterns.java
@@ -7,15 +7,17 @@ public final class Patterns {
     // GENERAL
     public static final Pattern SPACE = Pattern.compile(" ");
     public static final Pattern TILDE = Pattern.compile("~");
+    public static final Pattern COLON = Pattern.compile(":");
     public static final Pattern DOUBLE = Pattern.compile("\\.\\.");
     public static final Pattern ASTERISK = Pattern.compile("\\*");
 
-    // EFFECTS
-    public static final Pattern VARIABLE = Pattern.compile("(?<name>[a-zA-Z\\d]+)(?:=)(?<default>[\\d.-]+)");
+    // SCRIPTS
+    public static final Pattern SCRIPT = Pattern.compile("(?:\\[|(?<=,))(?<type>[\\w-]+)(=)(?<value>[a-zA-Z0-9{}=*.;_-]+)(?:(?=,)|])");
+    public static final Pattern INNER_SCRIPT = Pattern.compile("(?:\\{|(?<=;))(?<type>\\w+)(=)(?<value>[a-zA-Z\\d{}_.-]+)(?:(?=;)|})");
+    // VARIABLE SCRIPT
+    public static final Pattern VARIABLE = Pattern.compile("(?<name>[a-zA-Z\\d]+)(=)(?<default>[\\d.-]+)");
     public static final Pattern EVAL = Pattern.compile("^([a-zA-Z\\d]+)(\\X?)=(.+)$");
-    public static final Pattern SCRIPT = Pattern.compile("(?:\\[|(?<=\\,))(?<type>[\\w-]+)(?:=)(?<value>[a-zA-Z0-9{}=*.;_-]+)(?:(?=\\,)|\\])");
-    public static final Pattern INNER_SCRIPT = Pattern.compile("(?:\\{|(?<=;))(?<type>\\w+)(?:=)(?<value>[a-zA-Z\\d{}_.-]+)(?:(?=;)|})");
 
     // CONDITIONS
-    public static final Pattern CONDITIONAL = Pattern.compile("(?<condition>.+) (?:\\?) (?<first>.+) (?:\\:) (?<second>.+)");
+    public static final Pattern CONDITIONAL = Pattern.compile("(?<condition>.+) (\\?) (?<first>.+) (:) (?<second>.+)");
 }
