e17bdd80940b4b727d0823dc789aefae60d7cd20
ItsZypec <itszypec@gmail.com>
2023-09-29 19:02:55 +0300

    1.20.2 support

---
+++ v1_20_R2/src/main/java/net/treasure/version/v1_20_R2/NMSHandler.java
@@ -0,0 +1,101 @@
+package net.treasure.version.v1_20_R2;
+
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundBundlePacket;
+import net.minecraft.network.protocol.game.PacketListenerPlayOut;
+import net.minecraft.network.protocol.game.PacketPlayOutWorldParticles;
+import net.treasure.util.nms.AbstractNMSHandler;
+import net.treasure.util.nms.particles.ParticleBuilder;
+import net.treasure.util.nms.particles.ParticleEffect;
+import net.treasure.version.v1_20_R2.data.NMSGenericData;
+import net.treasure.version.v1_20_R2.data.color.NMSDustData;
+import net.treasure.version.v1_20_R2.data.color.NMSDustTransitionData;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.craftbukkit.v1_20_R2.CraftParticle;
+import org.bukkit.craftbukkit.v1_20_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class NMSHandler extends AbstractNMSHandler {
+
+    @Override
+    public void sendParticle(ParticleBuilder builder) {
+        var filter = builder.viewers();
+
+        var location = builder.location();
+        var packet = new PacketPlayOutWorldParticles(
+                builder.data(),
+                builder.longDistance(),
+                location.getX(),
+                location.getY(),
+                location.getZ(),
+                builder.offsetX(),
+                builder.offsetY(),
+                builder.offsetZ(),
+                builder.speed(),
+                builder.amount());
+
+        for (var player : Bukkit.getOnlinePlayers()) {
+            if (filter != null && !filter.test(player)) continue;
+
+            ((CraftPlayer) player).getHandle().c.b(packet);
+        }
+    }
+
+    @Override
+    public void sendParticles(List<ParticleBuilder> builders) {
+        List<Packet<PacketListenerPlayOut>> packets = new ArrayList<>();
+        Predicate<Player> filter = null;
+
+        for (var builder : builders) {
+            filter = builder.viewers();
+
+            var location = builder.location();
+            var packet = new PacketPlayOutWorldParticles(
+                    builder.data(),
+                    builder.longDistance(),
+                    location.getX(),
+                    location.getY(),
+                    location.getZ(),
+                    builder.offsetX(),
+                    builder.offsetY(),
+                    builder.offsetZ(),
+                    builder.speed(),
+                    builder.amount());
+
+            packets.add(packet);
+        }
+        var bundle = new ClientboundBundlePacket(packets);
+
+        for (var player : Bukkit.getOnlinePlayers()) {
+            if (filter != null && !filter.test(player)) continue;
+
+            ((CraftPlayer) player).getHandle().c.b(bundle);
+        }
+    }
+
+    @Override
+    public Object getParticleParam(ParticleEffect effect) {
+        var bukkit = effect.bukkit();
+        return bukkit == null ? null : CraftParticle.toNMS(bukkit);
+    }
+
+    @Override
+    public Object getDustData(Color color, float size) {
+        return new NMSDustData(color, size).toNMS();
+    }
+
+    @Override
+    public Object getColorTransitionData(Color color, Color transition, float size) {
+        return new NMSDustTransitionData(color, transition, size).toNMS();
+    }
+
+    @Override
+    public Object getGenericData(ParticleEffect effect, Object object) {
+        return new NMSGenericData(effect, object).toNMS();
+    }
+}
