d0397280bd276f97c59dbbbf674d9c894a774c16
ItsZypec <itszypec@gmail.com>
2024-02-20 14:45:48 +0300

    update pom

--- common/src/main/java/net/treasure/particles/effect/data/LocationEffectData.java
+++ common/src/main/java/net/treasure/particles/effect/data/LocationEffectData.java
@@ -1,6 +1,7 @@
 package net.treasure.particles.effect.data;
 
 import lombok.Getter;
+import lombok.Setter;
 import net.treasure.particles.TreasureParticles;
 import net.treasure.particles.effect.Effect;
 import net.treasure.particles.effect.handler.HandlerEvent;
@@ -11,8 +12,16 @@ public class LocationEffectData extends EffectData {
 
     private final String id;
     private Location location;
+    @Setter
+    private boolean persistent = true;
 
     public LocationEffectData(String id, Location location) {
+        this.id = id;
+        this.location = location;
+        this.currentEvent = HandlerEvent.STATIC;
+    }
+
+    public LocationEffectData(String id, Location location, boolean persistent) {
         this.id = id;
         this.location = location;
         this.currentEvent = HandlerEvent.STATIC;
@@ -31,19 +40,21 @@ public class LocationEffectData extends EffectData {
     @Override
     public boolean setCurrentEffect(Effect currentEffect) {
         var success = super.setCurrentEffect(currentEffect);
-        if (success)
+        if (success && persistent)
             TreasureParticles.getEffectManager().getStaticEffects().set(id, currentEffect == null ? null : currentEffect.getKey(), location);
         return success;
     }
 
     public void setLocation(Location location) {
         this.location = location;
-        TreasureParticles.getEffectManager().getStaticEffects().set(id, currentEffect == null ? null : currentEffect.getKey(), location);
+        if (persistent)
+            TreasureParticles.getEffectManager().getStaticEffects().set(id, currentEffect == null ? null : currentEffect.getKey(), location);
     }
 
     public void stop() {
         var effectManager = TreasureParticles.getEffectManager();
-        effectManager.getStaticEffects().set(id, null, null);
+        if (persistent)
+            effectManager.getStaticEffects().set(id, null, null);
         effectManager.getData().remove(id);
     }
 }
