321d3824576a4099e2f603d0a7d47a64e970d4ad
Emre Kaya <itszypec@gmail.com>
2025-01-26 15:24:54 +0300

    update text particle reader

--- common/src/main/java/net/treasure/particles/effect/script/particle/reader/text/TextParticleReader.java
+++ common/src/main/java/net/treasure/particles/effect/script/particle/reader/text/TextParticleReader.java
@@ -12,32 +12,39 @@ public class TextParticleReader extends ParticleReader<TextParticle> {
 
     public TextParticleReader() {
         super();
-
-        addValidArgument(c -> c.script().stepX(StaticArgument.asInt(c, 1)), "step-x");
-        addValidArgument(c -> c.script().stepY(StaticArgument.asInt(c, 1)), "step-y");
-        addValidArgument(c -> c.script().scale(StaticArgument.asFloat(c)), "scale");
-        addValidArgument(c -> c.script().tickData(StaticArgument.asBoolean(c)), "tick-data", "tick");
-        addValidArgument(c -> c.script().vertical(StaticArgument.asBoolean(c)), "vertical");
-        addValidArgument(c -> c.script().fontName(StaticArgument.asString(c)), "font");
-        addValidArgument(c -> c.script().text(StaticArgument.asString(c)), true, "text");
-
-        addValidArgument(c -> c.script().rotateX(StaticArgument.asFloat(c, 0, 360)), "rotate-x");
-        addValidArgument(c -> c.script().rotateY(StaticArgument.asFloat(c, 0, 360)), "rotate-y");
+        addValidArguments(this);
     }
 
-    @Override
-    public boolean validate(ParticleReader.Context<TextParticle> c) throws ReaderException {
-        if (!super.validate(c)) return false;
+    public static void addValidArguments(ParticleReader<? extends TextParticle> reader) {
+        reader.addValidArgument(c -> c.script().stepX(StaticArgument.asInt(c, 1)), "step-x");
+        reader.addValidArgument(c -> c.script().stepY(StaticArgument.asInt(c, 1)), "step-y");
+        reader.addValidArgument(c -> c.script().scale(StaticArgument.asFloat(c)), "scale");
+        reader.addValidArgument(c -> c.script().tickData(StaticArgument.asBoolean(c)), "tick-data", "tick");
+        reader.addValidArgument(c -> c.script().vertical(StaticArgument.asBoolean(c)), "vertical");
+        reader.addValidArgument(c -> c.script().fontName(StaticArgument.asString(c)), "font");
+        reader.addValidArgument(c -> c.script().text(StaticArgument.asString(c)), true, "text");
+
+        reader.addValidArgument(c -> c.script().rotateX(StaticArgument.asFloat(c, 0, 360)), "rotate-x");
+        reader.addValidArgument(c -> c.script().rotateY(StaticArgument.asFloat(c, 0, 360)), "rotate-y");
+    }
+
+    public static boolean validateContext(ParticleReader<? extends TextParticle> reader, ParticleReader.Context<? extends TextParticle> c) throws ReaderException {
         var script = c.script();
 
         var font = new Font(script.fontName(), Font.PLAIN, 16);
-        if (font.canDisplayUpTo(script.text()) != script.text().length()) {
-            error(c.effect(), c.type(), c.line(), "This text cannot be displayed properly with '" + script.fontName() + "' font");
+        if (font.canDisplayUpTo(script.text()) != -1) {
+            reader.error(c.effect(), c.type(), c.line(), "This text cannot be displayed properly with '" + script.fontName() + "' font");
             return false;
         }
 
         c.script().initialize();
         return true;
+    }
+
+    @Override
+    public boolean validate(ParticleReader.Context<TextParticle> c) throws ReaderException {
+        if (!super.validate(c)) return false;
+        return validateContext(this, c);
     }
 
     @Override
