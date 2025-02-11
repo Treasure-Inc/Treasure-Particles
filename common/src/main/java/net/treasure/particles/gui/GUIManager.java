85ddfe0527fc6de100da6b0727649ba79b4ead53
Emre Kaya <itszypec@gmail.com>
2025-02-11 16:27:04 +0300

    add random color selection button to colors gui

--- common/src/main/java/net/treasure/particles/gui/GUIManager.java
+++ common/src/main/java/net/treasure/particles/gui/GUIManager.java
@@ -27,7 +27,7 @@ import java.util.stream.Stream;
 @Getter
 public class GUIManager implements DataHolder {
 
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
 
     private final ConfigurationGenerator generator;
     private YamlConfiguration config;
