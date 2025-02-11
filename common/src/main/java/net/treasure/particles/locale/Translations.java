a731f081af9f3870c3062f66b81d1ee6b33f1568
Emre Kaya <itszypec@gmail.com>
2025-02-11 16:47:32 +0300

    update translations

--- common/src/main/java/net/treasure/particles/locale/Translations.java
+++ common/src/main/java/net/treasure/particles/locale/Translations.java
@@ -18,7 +18,7 @@ import java.util.Objects;
 public class Translations implements DataHolder {
 
     public static String LOCALE;
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
 
     private FileConfiguration config;
     @Getter
