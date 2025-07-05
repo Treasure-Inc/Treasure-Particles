22cd5da5ef7a5563fdf563f294fcba332d3ae290
Emre Kaya <itszypec@gmail.com>
2025-07-05 22:23:48 +0300

    add 1.21.7 support

--- common/src/main/java/net/treasure/particles/util/nms/NMSMatcher.java
+++ common/src/main/java/net/treasure/particles/util/nms/NMSMatcher.java
@@ -30,7 +30,7 @@ public class NMSMatcher {
         registerVersion("v1_21_R2", "21.2", "21.3");
         registerVersion("v1_21_R3", "21.4");
         registerVersion("v1_21_R4", "21.5");
-        registerVersion("v1_21_R5", "21.6");
+        registerVersion("v1_21_R5", "21.6", "21.7");
     }
 
     private static void registerVersion(String plugin, String... versions) {
