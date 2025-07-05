c119ec9695b184040acba4e28fb545458588e51d
Emre Kaya <itszypec@gmail.com>
2025-07-05 23:03:43 +0300

    update versions

--- common/src/main/java/net/treasure/particles/effect/EffectManager.java
+++ common/src/main/java/net/treasure/particles/effect/EffectManager.java
@@ -64,7 +64,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Getter
 public class EffectManager implements DataHolder {
 
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "1.4.2";
 
     private final ConcurrentHashMap<String, EffectData> data;
 
--- common/src/main/java/net/treasure/particles/locale/Translations.java
+++ common/src/main/java/net/treasure/particles/locale/Translations.java
@@ -18,7 +18,7 @@ import java.util.Objects;
 public class Translations implements DataHolder {
 
     public static String LOCALE;
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "1.4.2";
 
     private FileConfiguration config;
     @Getter
